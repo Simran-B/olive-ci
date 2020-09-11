34d48c72bbfe11fe93bbc697a84e49302ace9c67
Simran Brucherseifer <simran.brucherseifer@gmail.com>
2020-09-11 16:48:56 +0200

    FFMPEG CI image

--- scripts/base/install_ffmpeg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-yum install --setopt=tsflags=nodocs -y epel-release
-yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
-yum install --setopt=tsflags=nodocs -y ffmpeg ffmpeg-devel
