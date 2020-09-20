575d77c0bfe3abecf254ca2565a31ae27f417bd0
Simran Spiller <simran.spiller@gmail.com>
2020-09-21 01:30:00 +0200

    Crashpad cleanup

--- scripts/build_crashpad.sh
+++ scripts/build_crashpad.sh
@@ -11,35 +11,25 @@ git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools
 
 # HACK: Compile our own gn. The one included in depot_tools requires GLIBC_2.18,
 # but CentOS 7 only ships with GLIBC_2.17.
-# NOTE: Don't clone with --depth 1, this will make build/gen.py fail!
 git clone https://gn.googlesource.com/gn
+# NOTE: Don't clone with --depth 1, this will make build/gen.py fail!
 cd gn
 python build/gen.py
 ninja -C out
-# cp is typically an alias for cp -i (interactive), which -f does not override.
-# \cp or command cp will avoid the alias and use the underlaying command.
-#command cp -f out/gn ../depot_tools/gn
 cd ..
 # Put the path to our own gn build first
-export PATH="$(pwd)/gn/out:$(pwd)/depot_tools:$PATH"
+PATH="$(pwd)/gn/out:$(pwd)/depot_tools:$PATH"
+export PATH
 
 # Build Crashpad with Clang (default)
+# Toolchain can be controlled with env vars CC, CXX and AR
 mkdir crashpad
 cd crashpad
 fetch crashpad
 cd crashpad
+# TODO: Use gn args out/Default or edit out/Default.args.gn to configure build?
+# is_debug=true or target_cpu="x86"
 gn gen out/Default
 ninja -C out/Default
 
-# TODO: Use gn args out/Default or edit out/Default.args.gn to configure build?
-# is_debug=true or target_cpu="x86"
-
-#cd ../..
-#git clone --depth 1 https://github.com/olive-editor/olive.git
-#cd olive
-#mkdir build
-#cd build
-#cmake .. -G Ninja
-#cmake --build .
-#cmake --install app --prefix appdir/usr
-#/usr/local/linuxdeployqt-x86_64.AppImage appdir/usr/share/applications/org.olivevideoeditor.Olive.desktop -appimage --appimage-extract-and-run
+# TODO: Delete everyting we don't need in the package here?
