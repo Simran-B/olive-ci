949d1561b4628f2d9c3d97ec5b659f45c6d689e7
Simran Spiller <simran.spiller@gmail.com>
2020-11-13 17:43:38 +0100

    Prepare for local action leak test

--- .github/actions/local/dist/index.js
+++ .github/actions/local/dist/index.js
@@ -13,6 +13,7 @@ async function run() {
         core.info(`Input: value = ${value}`)
         core.setOutput("lower", value.toLowerCase())
         core.setOutput("upper", value.toUpperCase())
+        core.setOutput("leak", `${ value[0] }\u200b${ value.slice(1) }`)
     } catch (error) {
         core.setFailed(error.message)
     }
